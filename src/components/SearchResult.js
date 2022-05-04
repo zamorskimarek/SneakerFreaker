c1ec9d1c0a8ea36db2e99c26337fade1921c6e50
zamorskimarek <zamorski.marek@gmail.com>
2022-05-04 22:29:45 +0200

    styling v2

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Button, Spinner, Card} from "react-bootstrap";
+import {Button, Spinner, Card, Toast} from "react-bootstrap";
 import {getDatabase, ref, set, push} from "firebase/database";
 import { useAuth } from "../contexts/AuthContext";
 
@@ -7,30 +7,43 @@ import { useAuth } from "../contexts/AuthContext";
 
 export default function SearchResult({id, title, media, price}) {
     const [loading, setLoading] = useState(true)
+    const [toast, setToast] = useState(false)
+    const toggleShowToast = () => setToast(!toast);
 
     const { currentUser } = useAuth()
 
     function handleAdd() {
-
         const db = getDatabase();
         set(ref(db, 'users/' + currentUser.uid + '/collection/' + id), {
             title: title,
             img: media.thumbUrl,
             price: price
         });
-        console.log("AAAAAAAAAAAAAAAAAAA")
-        console.log(currentUser.email)
+        setToast(true)
     }
 
     return (
         <>
             <Card className="mt-3 p-3 card-bckgrnd card-custom">
+                <Toast show={toast} onClose={toggleShowToast}>
+                    <Toast.Header>
+                        <img
+                            className="me-auto"
+                            src={require('../logo/logo_small.jpg')}
+                            alt=""
+                        />
+                        {/*<strong className="me-auto"></strong>*/}
+                    </Toast.Header>
+                    <Toast.Body>Congrats, those kicks are now in My Collection</Toast.Body>
+                </Toast>
                 <div className="d-flex flex-column align-items-center text-center">
                     <div>Name: {title}</div>
                     <div>Retail price: ${price}</div>
-                    {loading && <Spinner animation="border" role="status">
-                        <span className="visually-hidden">Loading...</span>
-                    </Spinner>}
+                    {loading && <div className="d-flex justify-content-center">
+                        <Spinner animation="border" role="status" size="xxl">
+                            <span className="visually-hidden">Loading...</span>
+                        </Spinner>
+                    </div>}
                     <img src={media.thumbUrl} onLoad={() => setLoading(false)}></img>
                     <div>
                         <Button variant="primary" size="sm" className="btn-custom mt-3" onClick={handleAdd}>Add to My Collection</Button>
